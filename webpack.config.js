5591bc1f0346032bddc6e553d87dc9a9c003c5c9
Tatty13 <tat.mi.iva@gmail.com>
2022-08-25 19:00:33 +0300

    feat: add webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+let path = require('path');
+const { config } = require('process');
+
+let conf = {
+
+  entry: path.resolve(__dirname, './src/index.js'),
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./"),
+      watch: true
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+}
+
+module.exports = (env, options) => {
+  let isProd = options.mode === 'production';
+  conf.devtool = isProd ? false : 'eval-source-map';
+  return conf;
+};
